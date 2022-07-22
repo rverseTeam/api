cdf05982058594d6f71c51043497dce0f231b64f
Repflez <659133+Repflez@users.noreply.github.com>
2022-07-22 06:24:32 -0600

    chore: Apply EditorConfig rules to the rest of the project

--- rverse.Services/LoggingService/LoggerManager.cs
+++ rverse.Services/LoggingService/LoggerManager.cs
@@ -8,14 +8,14 @@ namespace rverse.Services.LoggingService;
 
 public class LoggerManager : ILoggerManager
 {
-    private static ILogger logger = LogManager.GetCurrentClassLogger();
+    private static readonly ILogger s_logger = LogManager.GetCurrentClassLogger();
     public LoggerManager()
     {
     }
 
-    public void LogDebug(string message) => logger.Debug(message);
-    public void LogError(string message) => logger.Error(message);
-    public void LogInfo(string message) => logger.Info(message);
-    public void LogWarn(string message) => logger.Warn(message);
+    public void LogDebug(string message) => s_logger.Debug(message);
+    public void LogError(string message) => s_logger.Error(message);
+    public void LogInfo(string message) => s_logger.Info(message);
+    public void LogWarn(string message) => s_logger.Warn(message);
 
 }
